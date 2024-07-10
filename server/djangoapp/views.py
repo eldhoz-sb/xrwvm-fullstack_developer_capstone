b0650e7076048a37dbeddc823b7c9fbd26ce736d
eldhoz-sb <110928118+eldhoz-sb@users.noreply.github.com>
2024-07-10 14:02:51 +0530

    commit after fixing lint errors 3

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -91,7 +91,8 @@ def registration(request):
         return JsonResponse(data)
 
 
-# Update the `get_dealerships` render list of dealerships all by default, particular state if state is passed
+# Update the `get_dealerships` render list of dealerships all by default,
+# particular state if state is passed
 def get_dealerships(request, state="All"):
     if (state == "All"):
         endpoint = "/fetchDealers"
@@ -131,13 +132,13 @@ def get_dealer_details(request, dealer_id):
 
 
 def add_review(request):
-    if (request.user.is_anonymous == False):
+    if not request.user.is_anonymous:
         data = json.loads(request.body)
         try:
             response = post_review(data)
-            return JsonResponse({"status": 200})
-        except:
-            return JsonResponse({"status": 401, "message": "Error in posting review"})
+            return JsonResponse({"status": 200, "response": response})
+        except Exception as e:
+            return JsonResponse({"status": 401,"message": "Error in posting review"})
     else:
         return JsonResponse({"status": 403, "message": "Unauthorized"})
 
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -31,13 +31,20 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
-    'eldhosesaji2-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai'
+    (
+        'eldhosesaji2-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+        'proxy.cognitiveclass.ai'
+    ),
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://eldhosesaji2-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai',
-    'http://127.0.0.1'
+    (
+        'https://eldhosesaji2-8000.theiadockernext-0-labs-prod-theiak8s-4-tor01.'
+        'proxy.cognitiveclass.ai'
+    ),
+    'http://127.0.0.1',
 ]
+
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -101,8 +108,10 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
     {
         'NAME':
